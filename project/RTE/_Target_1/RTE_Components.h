a3a0d90c416f727969efcbc6ad613c2463464ebd
HRex <475273498@qq.com>
2021-06-18 22:01:06 +0800

    update .gitignore to avoid .hex and so on

--- project/RTE/_Target_1/RTE_Components.h
+++ project/RTE/_Target_1/RTE_Components.h
@@ -16,21 +16,21 @@
  */
 #define CMSIS_device_header "stm32f10x.h"
 
-/*  Keil::CMSIS Driver:USART:2.3 */
+/* Keil::CMSIS Driver:USART:2.3 */
 #define RTE_Drivers_USART1              /* Driver USART1 */
         #define RTE_Drivers_USART2              /* Driver USART2 */
         #define RTE_Drivers_USART3              /* Driver USART3 */
         #define RTE_Drivers_USART4              /* Driver UART4  */
         #define RTE_Drivers_USART5              /* Driver UART5  */
-/*  Keil::Device:StdPeriph Drivers:Framework:3.5.1 */
+/* Keil::Device:StdPeriph Drivers:Framework:3.5.1 */
 #define RTE_DEVICE_STDPERIPH_FRAMEWORK
-/*  Keil::Device:StdPeriph Drivers:GPIO:3.5.0 */
+/* Keil::Device:StdPeriph Drivers:GPIO:3.5.0 */
 #define RTE_DEVICE_STDPERIPH_GPIO
-/*  Keil::Device:StdPeriph Drivers:RCC:3.5.0 */
+/* Keil::Device:StdPeriph Drivers:RCC:3.5.0 */
 #define RTE_DEVICE_STDPERIPH_RCC
-/*  Keil::Device:StdPeriph Drivers:TIM:3.5.0 */
+/* Keil::Device:StdPeriph Drivers:TIM:3.5.0 */
 #define RTE_DEVICE_STDPERIPH_TIM
-/*  Keil::Device:StdPeriph Drivers:USART:3.5.0 */
+/* Keil::Device:StdPeriph Drivers:USART:3.5.0 */
 #define RTE_DEVICE_STDPERIPH_USART
 
 
